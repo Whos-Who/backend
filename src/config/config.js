ae73a6f05a1f69b28cc8c25952ec82bd62d66535
zatkiller <gheedylan@yahoo.com.sg>
2021-09-08 02:43:39 +0800

    Add dialect to PSQL production config

--- src/config/config.js
+++ src/config/config.js
@@ -10,6 +10,7 @@ module.exports = {
     dialect: 'postgres'
   },
   production: {
-    use_env_variable: process.env.DATANASE_URL
+    use_env_variable: process.env.DATANASE_URL,
+    dialect: 'postgres'
   }
 };
