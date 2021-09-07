5f98cf6c143fb5b80ffad4088cefccf4dffe10b9
zatkiller <gheedylan@yahoo.com.sg>
2021-09-08 03:01:52 +0800

    Remove dialects

--- src/config/config.js
+++ src/config/config.js
@@ -1,16 +1,13 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
+const DATABASE_URL = process.env.DATABASE_URL;
+
 module.exports = {
   development: {
-    username: 'postgres',
-    database: 'assignment-3',
-    host: '127.0.0.1',
-    port: '5432',
-    dialect: 'postgres'
+    url: DATABASE_URL
   },
   production: {
-    url: process.env.DATABASE_URL,
-    dialect: 'postgres'
+    url: DATABASE_URL
   }
 };
