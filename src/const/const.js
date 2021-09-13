bb55973cdad106040be0606c25fa3994a1712f61
zatkiller <gheedylan@yahoo.com.sg>
2021-09-13 22:09:17 +0800

    Add development node env

--- src/const/const.js
+++ src/const/const.js
@@ -4,4 +4,4 @@ dotenv.config();
 export const REDIS_URL = process.env.REDIS_URL || '';
 export const DATABASE_URL = process.env.DATABASE_URL;
 export const PORT = process.env.PORT || 5000;
-export const NODE_ENV = process.env.NODE_ENV;
+export const NODE_ENV = process.env.NODE_ENV || 'development';
