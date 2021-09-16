354e1f13987c53a7d24509fa8ea03cef28dc1463
Wen Hao <52305097+wenhaogoh@users.noreply.github.com>
2021-09-16 12:01:47 +0800

    Fix retrieve all decks route to be user specific (#25)

--- src/database/migrations/20210911020034-create-question.js
+++ src/database/migrations/20210911020034-create-question.js
@@ -12,6 +12,7 @@ module.exports = {
         type: Sequelize.STRING
       },
       deckId: {
+        allowNull: false,
         type: Sequelize.INTEGER,
         references: {
           model: 'Decks',
