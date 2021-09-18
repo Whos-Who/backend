80887d4039c2a3e705534b1ad0da65be070a766d
zatkiller <gheedylan@yahoo.com.sg>
2021-09-19 01:17:03 +0800

    Add seed migration command to script

--- src/const/const.js
+++ src/const/const.js
@@ -6,4 +6,4 @@ export const DATABASE_URL = process.env.DATABASE_URL;
 export const PORT = process.env.PORT || 5000;
 export const NODE_ENV = process.env.NODE_ENV || 'development';
 export const TOKEN_KEY = process.env.TOKEN_KEY || 'secret';
-export const ON_AUTH = process.env.ON_AUTH || false;
+export const ON_AUTH = Boolean(process.env.ON_AUTH) || false;
