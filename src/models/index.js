3842e57bfdc04fd6945afb8d681e2ad496c36d60
zatkiller <gheedylan@yahoo.com.sg>
2021-09-19 00:52:22 +0800

    Fix NODE_ENV env variable

--- src/models/index.js
+++ src/models/index.js
@@ -1,10 +1,13 @@
 'use strict';
 
+const dotenv = require('dotenv');
+dotenv.config();
+
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env = NODE_ENV || 'development';
+const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
