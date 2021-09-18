254e9913980ed0a23da914ad8f77dc412d07b84b
zatkiller <gheedylan@yahoo.com.sg>
2021-09-19 00:56:53 +0800

    Add seeder file for introdution duck

---
+++ src/database/seeders/20210918165259-introduction-deck.js
@@ -0,0 +1,97 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const decks = await queryInterface.bulkInsert(
+      'Decks',
+      [
+        {
+          title: 'Introduction questions for stranges',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+      ],
+      { returning: true }
+    );
+
+    const deckId = decks[0]['id'];
+
+    await queryInterface.bulkInsert('Questions', [
+      {
+        question: 'What’s your favorite way to spend a day off?',
+        deckId: deckId,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        question: 'What type of music are you into?',
+        deckId: deckId,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        question: 'What was the best vacation you ever took and why?',
+        deckId: deckId,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        question: 'Where’s the next place on your travel bucket list and why?',
+        deckId: deckId,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        question: 'What are your hobbies, and how did you get into them?',
+        deckId: deckId,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        question: 'What was the last thing you read?',
+        deckId: deckId,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        question: 'Would you say you’re more of an extrovert or an introvert?',
+        deckId: deckId,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        question:
+          'If you could only eat one food for the rest of your life, what would it be?',
+        deckId: deckId,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        question: 'What’s your secret guilty pleasure?',
+        deckId: deckId,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        question: 'Do you have any pet peeves?',
+        deckId: deckId,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    const decks = await queryInterface.bulkDelete(
+      'Decks',
+      {
+        userId: null,
+        title: 'Introduction questions for stranges'
+      },
+      {
+        returning: true
+      }
+    );
+
+    // Questions will be deleted due to cascade
+  }
+};
