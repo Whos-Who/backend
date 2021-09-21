2bdcc4e6f2fcee5a0549dbad48da3e7638e73820
wenhao <wenhaogoh1998@gmail.com>
2021-09-22 01:31:40 +0800

    Hotfix room prefix

--- src/controllers/room.js
+++ src/controllers/room.js
@@ -1,10 +1,11 @@
 import { StatusCodes } from 'http-status-codes';
 import { redisClient } from '../database/redis';
+import { ROOM_PREFIX } from '../const/redis';
 
 async function checkRoomExist(req, res, next) {
   try {
     const roomCode = req.params.roomCode;
-    const exists = await redisClient.exists(`ROOM_PREFIX-${roomCode}`);
+    const exists = await redisClient.exists(`${ROOM_PREFIX}-${roomCode}`);
     if (exists == 0) {
       return res.status(StatusCodes.NOT_FOUND).send();
     }
