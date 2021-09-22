ff7227d80c49cd8bb419446f07681d587dff31f2
Wen Hao <52305097+wenhaogoh@users.noreply.github.com>
2021-09-22 16:03:25 +0800

    Change room check route (#53)
    * Change room check route
    
    * Change function name

--- src/controllers/room.js
+++ src/controllers/room.js
@@ -2,17 +2,18 @@ import { StatusCodes } from 'http-status-codes';
 import { redisClient } from '../database/redis';
 import { ROOM_PREFIX } from '../const/redis';
 
-async function checkRoomExist(req, res, next) {
+async function retrieveRoom(req, res, next) {
   try {
     const roomCode = req.params.roomCode;
     const exists = await redisClient.exists(`${ROOM_PREFIX}-${roomCode}`);
     if (exists == 0) {
       return res.status(StatusCodes.NOT_FOUND).send();
     }
-    res.status(StatusCodes.NO_CONTENT).send();
+    const phase = await redisClient.hget(`${ROOM_PREFIX}-${roomCode}`, 'phase');
+    res.status(StatusCodes.OK).json({ phase: phase });
   } catch (err) {
     next(err);
   }
 }
 
-export const checkRoomExistFunc = [checkRoomExist];
+export const showRoomFunc = [retrieveRoom];
